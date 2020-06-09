109fbe17d8e411a4d805a1e4e5ac43f433c7e78e
Thomas Kushner <61517003+s276992@users.noreply.github.com>
2020-06-09 15:06:22 -0400

    assignment 6

--- Assignment6TK/Assignment6/splash_screen.lua
+++ Assignment6TK/Assignment6/splash_screen.lua
@@ -1,96 +1,132 @@
 -- Assignment 6
--- Created by: Thomas Kushner
+-- Thomas Kushner
 -- ICS2O
--- Description: This is the splash screen of the game. It displays the 
--- company logo that...
------------------------------------------------------------------------------------------
+-- Description: This displays my company logo on the splash screen.
+---------------------------------------------------------------------
 
--- Use Composer Library
-local composer = require( "composer" )
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
 
--- Name the Scene
-sceneName = "splash_screen"
+-- create background 
+local backgroundImage = display.newImageRect("Images/bkg.png", 2048, 1536)
 
------------------------------------------------------------------------------------------
+-- Create the objects in the splash screen
+local jet = display.newImageRect("Images/jet.png", 200, 200)
+local panzer = display.newImageRect("Images/panzer.png", 400, 400)
+local logo = display.newImageRect("Images/tLogo.png", 1050, 836)
 
--- Create Scene Object
-local scene = composer.newScene( sceneName )
+---------------------------------------------------------------------
+-- SOUNDS
+---------------------------------------------------------------------
+local trumpet = audio.loadSound("Sounds/horn.mp3")
+local hornSoundChannel
 
-----------------------------------------------------------------------------------------
--- LOCAL VARIABLES
------------------------------------------------------------------------------------------
- 
--- The local variables for this scene
-local logo
-local jet
-local panzer
-local scrollXSpeed = 8
-local scrollYSpeed = -3
-local trumpetSound = audio.loadSound("Sounds/horn.mp3")
-local trumpetSoundsChannel
+---------------------------------------------------------------------
 
---------------------------------------------------------------------------------------------
--- LOCAL FUNCTIONS
---------------------------------------------------------------------------------------------
 
--- The function that moves the beetleship across the screen
-local function moveJet( event )
-    jet.x = jet.x + scrollXSpeed
-    jet.y = jet.y + scrollYSpeed
-end
+-- set the intial (x,y) position of jet
+jet.x = 100
+jet.y = display.contentHeight/7
 
--- The function that changes the transparency of the panzer
-local function fadePanzer( event )
-    panzer.alpha = panzer.alpha - 0.01
-end
 
--- This funtion moves the panzer
-local function movePanzer ( event )
-    panzer.x = panzer.x + scrollXSpeed
-    panzer.y = panzer.y + scrollYSpeed
-end
-   
--- This function fades out the logo
-local function FadeLogo( event )
-    logo.alpha = logo.alpha - 0.001
-end
--- The function that will go to the main menu 
-local function gotoMainMenu( event )
+-- set the initial (x,y) position of panzer
+panzer.x = 350
+panzer.y = display.contentHeight/1.7
+
+-- set the initial (x,y) position of logo
+logo.x = 500
+logo.y = display.contentHeight/2
+logo.isVisible = false
+
+
+-- global variables
+scrollSpeed = 10
+
+--------------------------------------------------------------------
+-- FUNCTIONS
+--------------------------------------------------------------------
+
+-- Function: MoveJet
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: this function adds the scroll speed to the x-value of squid
+local function MoveJet(event)  
+    -- add the scroll speed to the x-value of squid
+    jet.x = jet.x + scrollSpeed
     
+    -- play audio
+    hornSoundChannel = audio.play( trumpet )
 end
+
+Runtime:addEventListener("enterFrame", MoveJet)
+
+-- Function: MovePanzer
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: This changes the position of an object
+local function MovePanzer(event)
+    -- move the tank
+    panzer.x = panzer.x - scrollSpeed
+end
+
+Runtime:addEventListener("enterFrame", MovePanzer)
+
+-- Function: FadePanzer
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: This changes the transparency of an object
+local function FadePanzer(event)
+    -- change the transparency of the object
+    panzer.alpha = panzer.alpha - 0.02
+end
+
+Runtime:addEventListener("enterFrame", FadePanzer)
+
+-- Function: LOGO
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: This changes the transparency of an object
+local function LOGO( event )
+    -- change the transparency of the object
+    logo.alpha = logo.alpha + 0.01
+end
+
+-- stop playing the trumpet
+
+hornSoundChannel = audio.stop( trumpet )
 
 -----------------------------------------------------------------------------------------
 -- GLOBAL SCENE FUNCTIONS
 -----------------------------------------------------------------------------------------
 
--- The function called when the screen doesn't exist
 function scene:create( event )
 
     -- Creating a group that associates objects with the scene
     local sceneGroup = self.view
 
-    display.newImageRect("Images/logo.png", 1000, 500)
-    display.newImageRect("Images/jet.png", 200, 100)
-    display.newImageRect("Images/panzer.png", 300, 200)
+    -- Insert the background image
+   backgroundImage = display.newImageRect("Images/bkg.png", 2048, 1536)
+   backgroundImage.x = display.contentWidth / 2
+   backgroundImage.y = display.contentHeight / 2
 
-    -- set the initial x and y position of the objects
-    logo.x = 500
-    logo.y = contentHeight/2
+    -- Insert the jet
+    jet = display.newImageRect("Images/jet.png", 200, 200)
+    jet.x = display.contentWidth * 3 / 5
+    jet.y = display.contentWidth * 3.5 / 5
 
-    jet.x = 200
-    jet.y = contentHeight/5
+    -- Insert the panzer
 
-    panzer.x = 200
-    panzer.y = contentHeight/7
+    panzer = display.newImageRect("Images/panzer.png", 400, 400)
+    panzer.x = display.contentWidth/5 
+    panzer.y = display.contentHeight*6.1/7
 
-    -- Insert objects into the scene group in order to ONLY be associated with this scene
-    sceneGroup:insert( logo )
-    sceneGroup:insert( jet )
-    sceneGroup:insert( panzer )
+    -- Inset the logo
 
-end 
+    logo = display.newImageRect("Images/tLogo.png", 1050, 836)
+    logo.x = display.contentWidth /2
+    logo.y = display.contentHeight/2
 
---------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------
 
 -- The function called when the scene is issued to appear on screen
 function scene:show( event )
@@ -108,37 +144,54 @@ function scene:show( event )
     if ( phase == "will" ) then
        
     -----------------------------------------------------------------------------------------
-
-    elseif ( phase == "did" ) then
-        -- start the splash screen music
-        trumpetSoundsChannel = audio.play(trumpetSound )
-
+        -- start the physics engine
+        physics.start()
     
+    elseif ( phase == "did" ) then
+
+        --Make the logo dynamic so that it will move
+        physics.addBody(logo, "dynamic", {density=0.04, friction=0})
+
+        logo:applyForce( 0, 1000, logo.x, logo.y )
+
+        --make the bottom static so that it won't move
+        physics.addBody(bottom, "static")
+
+        -- Call the GameStart function as soon as we enter the frame.
+        SplashStart( )
 
         -- Go to the main menu screen after the given time.
-        timer.performWithDelay ( 3000, gotoMainMenu)          
+        timer.performWithDelay ( 4500, gotoMainMenu)          
         
     end
 
-end 
+end --function scene:show( event )
+
 -----------------------------------------------------------------------------------------
 
 -- The function called when the scene is issued to leave the screen
-local function scene hide( event )
+function scene:hide( event )
 
     -- Creating a group that associates objects with the scene
     local sceneGroup = self.view
-    local phase = event.phase
+    local phase = event.phase 
 
-    -----------------------------------------------------------------------------------------
+    -- Called when the scene is still off screen (but is about to come on screen).
+    if ( phase == "will" ) then
 
-    -- Called immediately after scene goes off screen.
-    elseif( phase == "did" ) then
+        --Make the logo dynamic so that it will move
+        physics.removeBody(logo)
+
+        --make the bottom static so that it won't move
+        physics.removeBody(bottom)
         
-        -- stop the jungle sounds channel for this screen
-        audio.stop(trumpetSoundsChannel)
+    elseif ( phase == "did") then
+
+        physics.stop()
     end
 
+    -----------------------------------------------------------------------------------------
+end --function scene:hide( event )
 
 -----------------------------------------------------------------------------------------
 
@@ -148,17 +201,16 @@ function scene:destroy( event )
     -- Creating a group that associates objects with the scene
     local sceneGroup = self.view
 
-    
 -----------------------------------------------------------------------------------------
 -- EVENT LISTENERS
 -----------------------------------------------------------------------------------------
 
 -- Adding Event Listeners
-scene:addEventListener( "create", moveJet )
-scene:addEventListener( "show", fadePanzer )
-scene:addEventListener( "hide", movePanzer )
-scene:addEventListener( "destroy", FadeLogo )
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
 
------------------------------------------------------------------------------------------
+----------------------------------------------------------------------
 
 return scene
